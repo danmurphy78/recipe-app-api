4a2aaa98634e4587ccb6a37a61519f0f3da8fb0d
Dan Murphy <danmurphy@Dans-MacBook-Pro.local>
2019-08-13 11:22:52 +0100

    Fixed admin test

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,35 +1,28 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-import logging
+from django.test import Client
 
 
 class AdminSiteTests(TestCase):
 
-    def setuUp(self):
+    def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@google.com',
-            password='test123'
+            email='admin@londonappdev.com',
+            password='password123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@google.com',
-            password='test123',
-            name='Test user full name'
+            email='test@londonappdev.com',
+            password='password123',
+            name='Test User Full Name',
         )
 
     def test_users_listed(self):
-        """Test that users are listed on user page"""
+        """Test that users are listed on the user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
-
-        logging.basicConfig(level=logging.DEBUG)
-        logging.debug('--- - - -- - -- - -')
-        logging.debug(res)
-        logging.debug('--- - - -- - -- - -')
-        logging.debug(dir(res))
-        logging.debug('--- - - -- - -- - -')
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
